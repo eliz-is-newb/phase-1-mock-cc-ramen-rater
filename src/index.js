58703d39e89132c0b35c20e30bdcede90cd4a4ee
Sky DeGrate <skydegrate@gmail.com>
2022-07-26 11:32:38 -0400

    help

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-// write your code here
+// mandatory executables:
+// [] GET request the API
+// [] Listen event handlers and updating
+//    the DOM
+
+
+
+
+// 
+
+//function fetching(forEach something
+//   input the fetched data into:
+//   also: name and restaurant name
+//   comment:
+//   rating:
+// )
+
+
+
+
+//fetch setting
+ramen = "http://localhost:3000/ramens"
+ramenId = "http://localhost:3000/ramens/:id"
+
+ fetch(ramen)
+  .then(res => res.json())
+  .then(data => Object.key.forEach((ramenImage) => {
+    
+    //first up: ramen folder
+const div = document.createElement('div');
+div.setAttribute("id", "ramen-menu");
+
+//ramen images
+const img = document.createElement("img");
+div.append(img); 
+console.log(div);
+
+//ramen photo info
+const r = document.getElementById('rating-display');
+const c = document.getElementById('comment-display');
+r.innerText = " ";
+c.innerText = " ";
+
+})
+);
+
+
+
+
+
